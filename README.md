d92bb730f4b85e41e439e85c3f79a73f128c41b3
jwin4740@gmail.com <jwin4740@gmail.com>
2022-03-26 11:22:44 -0400

    pinned tweet post in progress

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # py-twitter
+
+#Tasks
+REFERENCE
+https://github.com/twitterdev/getting-started-with-the-twitter-api-v2-for-academic-research
+
+1 Get a user's pinned tweet and respond
--- app.py
+++ app.py
@@ -1,5 +1,7 @@
+from collections import namedtuple
 from typing import List, Tuple
 from utils import *
+import sys
 
 import tweepy
 from decouple import config
@@ -20,13 +22,14 @@ DB_PASSWORD = config('DB_PASSWORD')
 USERNAME = 'veritanonbugia'
 USER_ID = 1457371398507220993
 
+UserPinnedResponse = namedtuple("UserPinnedResponse", ("user_id", "username", "pinned_tweet"))
+
 
 def select():
     return f"SELECT * FROM tweets WHERE used = False ORDER BY created_at ASC;"
 
 
 Conn = start_db(DB_HOST, DB_USER, DB_PASSWORD)
-print(Conn)
 
 
 def return_text_hashtags():
@@ -55,11 +58,16 @@ class SimpleTwitter:
         self.api = tweepy.Client(bearer_token=bearer_token, consumer_key=api_key, consumer_secret=api_secret,
                                  access_token=access_token, access_token_secret=access_token_secret)
 
-    def get_user(self):
-        return self.api.get_user(username=USERNAME)
+    def get_user_id_with_pinned_tweet(self, username='veritanonbugia') -> UserPinnedResponse:
+        res: tweepy.Response = self.api.get_user(username=username, expansions='pinned_tweet_id')
+        d = {'user_id': res.data.id, 'username': res.data.username, 'pinned_tweet': res.includes['tweets'][0]}
 
-    def get_user_tweets(self):
-        return self.api.get_users_tweets(id=USER_ID)
+        return d
+
+    # gets 10 most recent tweets
+    def get_user_tweets(self, user_id=1457371398507220993):
+        user_tweets = self.api.get_users_tweets(id=user_id, max_results=5)
+        return user_tweets
 
     def tweet(self, raw_text):
         try:
@@ -68,6 +76,14 @@ class SimpleTwitter:
             self.api.create_tweet(text=final_text)
             print('successfully created tweet')
             mark_tweet_true(raw_text[0])
+        except Exception as E:
+            print(E)
+
+    def tweet_pinned(self, raw_text, pinned_id):
+        try:
+            self.api.create_tweet(text=raw_text, in_reply_to_tweet_id=pinned_id)
+            print('successfully created tweet')
+
         except Exception as E:
             print(E)
 
@@ -88,7 +104,15 @@ class SimpleTwitter:
 
 #
 client = SimpleTwitter()
-client.tweet(return_text_hashtags())
+
+# usr = client.get_user_id_with_pinned_tweet()
+# print(usr.includes)
+
+# twts = client.get_user_tweets()
+# print(len(twts))
+# print(twts)
+# client.tweet(return_text_hashtags())
+client.tweet_pinned('test', '1504832968883322909')
 
 # public_tweets = api.home_timeline()
 # for tweet in public_tweets:
